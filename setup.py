d914715158bc4580eb8ce6d47ad3f1ce0255802e
Chris McDonough <chrism@agendaless.com>
2008-12-10 22:53:31 +0000

    Bump version for release.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except (IOError, OSError):
     CHANGES = ''
 
 setup(name='superlance',
-      version='0.2',
+      version='0.3',
       description='superlance plugins for supervisord',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
