0bd2408b4bd374a2be26721fe251c0b6dfd4f9cb
Mike Naberezny <mike@naberezny.com>
2013-01-17 16:52:07 -0800

    Add Python version detection to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import os
+import sys
+
+if sys.version_info[:2] < (2, 4) or sys.version_info[0] > 2:
+    msg = ("Superlance requires Python 2.4 or later but does not work on "
+           "any version of Python 3.  You are using version %s.  Please "
+           "install using a supported version." % sys.version)
+    sys.stderr.write(msg)
+    sys.exit(1)
+
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
