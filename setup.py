7e9ac28d8f066d6d7be363873f4e6df8f96d95c2
Mike Naberezny <mike@naberezny.com>
2013-09-18 09:50:22 -0700

    Prepare 0.9 release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except (IOError, OSError):
     CHANGES = ''
 
 setup(name='superlance',
-      version='0.8',
+      version='0.9',
       license='BSD-derived (http://www.repoze.org/LICENSE.txt)',
       description='superlance plugins for supervisord',
       long_description=README + '\n\n' +  CHANGES,
