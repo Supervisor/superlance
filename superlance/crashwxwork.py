937aa993083275532f0a955a787b1de4667c17c2
PHP-庞小龙 <pangxiaolong@lanyife.com>
2021-02-18 16:51:35 +0800

    feta: 实现企业微信支持

--- superlance/crashwxwork.py
+++ superlance/crashwxwork.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python -u
+# -*- coding:utf-8
 ##############################################################################
 #
 # Copyright (c) 2007 Agendaless Consulting and Contributors.
